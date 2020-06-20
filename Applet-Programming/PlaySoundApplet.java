bf258443e31abd354f3c5ec1d14b00de8c2beac4
Tarun Sehgal <tarunsehgal27@gmail.com>
2020-06-20 12:31:43 +0530

    play_audio_clip

---
+++ Applet-Programming/PlaySoundApplet.java
@@ -0,0 +1,26 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class PlaySoundApplet extends Applet implements ActionListener {
+   Button play,stop;
+   AudioClip audioClip;
+   
+   public void init() {
+      play = new Button(" Play in Loop ");
+      add(play);
+      play.addActionListener(this);
+      stop = new Button(" Stop ");
+      add(stop);
+      stop.addActionListener(this);
+      audioClip = getAudioClip(getCodeBase(), "Chronicle Of Chandigarh PG - Satinder Sartaaj (DJJOhAL.Com)");
+   }
+   public void actionPerformed(ActionEvent ae) {
+      Button source = (Button)ae.getSource();
+      if (source.getLabel() == " Play in Loop ") {
+         audioClip.play();
+      } else if(source.getLabel() == "  Stop  "){
+         audioClip.stop();
+      }
+   }
+}
